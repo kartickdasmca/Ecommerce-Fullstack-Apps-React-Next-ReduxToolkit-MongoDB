bc3375ddac3485d9cc9529455ebfd248a044a572
kartickdasmca <kartickdasdk@gmail.com>
2025-04-30 17:36:07 +0530

    vercel

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
 import db from '../../lib/db'
 import logger from "../../utils/logger";
 export default function handler(req, res) {
