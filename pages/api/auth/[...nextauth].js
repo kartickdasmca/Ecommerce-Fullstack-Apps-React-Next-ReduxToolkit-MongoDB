2a001b60c9b58bed5286d83c9e9de586c33b0293
kartickdasmca <kartickdasdk@gmail.com>
2025-05-03 21:32:49 +0530

    fix deployment issue 5

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -37,7 +37,7 @@ export const authOptions = {
   session:{
     strategy : "jwt"
   },
-  secret : process.env.JWT_SECRET
+  secret : process.env.NEXTAUTH_SECRET
 }
 
 export default NextAuth(authOptions)
